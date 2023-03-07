c775e32093a6789e25efe7035ebaaaee5c06e7fa
Gisele <gisele_sig@hotmail.com>
2023-03-06 21:20:47 -0300

    conf iniciais activity

---
+++ app/src/main/java/br/com/xlcode/orgs/MainActivity.kt
@@ -0,0 +1,21 @@
+package br.com.xlcode.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de Frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maçã"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
+    }
+}
